8a3465c96b1a6ba87aeb39d7813c211d6bfa1d54
Robert Novotny <novotnr0@gmail.com>
2020-03-21 14:12:16 +0100

    Pridany ListView a adapter do kodu aktivity

--- app/src/main/java/com/github/novotnyr/android/presentr/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/presentr/MainActivity.java
@@ -1,14 +1,26 @@
 package com.github.novotnyr.android.presentr;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.*;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.*;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.userListView)
+    ListView userListView;
+
+    ArrayAdapter<User> userListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        userListViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        userListView.setAdapter(userListViewAdapter);
     }
 }
